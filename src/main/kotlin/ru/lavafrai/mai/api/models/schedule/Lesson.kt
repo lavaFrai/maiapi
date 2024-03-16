da6c87d97347de6b010781db7f8b60ed64bb136c
lava_frai <lavafrai@yandex.ru>
2024-03-17 00:28:26 +0300

    Lesson.getPairNumber Lesson.timeRange

--- src/main/kotlin/ru/lavafrai/mai/api/models/schedule/Lesson.kt
+++ src/main/kotlin/ru/lavafrai/mai/api/models/schedule/Lesson.kt
@@ -15,4 +15,18 @@ data class Lesson(
     val lms: String,
     val teams: String,
     val other: String,
-)
+    val timeRange: String = "${timeStart.time.substring(0, timeStart.time.indexOf(":", 3))} – ${timeEnd.time.substring(0, timeEnd.time.indexOf(":", 3))}"
+) {
+    fun getPairNumber(): Int {
+        return when (timeRange) {
+            "09:00 – 10:30" -> 1
+            "10:45 – 12:15" -> 2
+            "13:00 – 14:30" -> 3
+            "14:45 – 16:15" -> 4
+            "16:30 – 18:00" -> 5
+            "18:15 – 19:45" -> 6
+            "20:00 – 21:30" -> 7
+            else -> 0
+        }
+    }
+}
--- src/main/kotlin/ru/lavafrai/mai/api/models/schedule/ScheduleDay.kt
+++ src/main/kotlin/ru/lavafrai/mai/api/models/schedule/ScheduleDay.kt
@@ -1,5 +1,6 @@
 package ru.lavafrai.mai.api.models.schedule
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ru.lavafrai.mai.api.models.time.Date
 import ru.lavafrai.mai.api.models.time.DayOfWeek
@@ -7,6 +8,6 @@ import ru.lavafrai.mai.api.models.time.DayOfWeek
 @Serializable
 data class ScheduleDay(
     val date: Date? = null,
-    val day: DayOfWeek,
+    @SerialName("day") val dayOfWeek: DayOfWeek,
     val lessons: List<Lesson>
 )
