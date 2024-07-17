21193254538f8c3095ec1b0c9cef3edd90854d20
lavafrai <lavafrai@yandex.ru>
2024-07-17 18:38:06 +0300

    Migration to kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.0.0"
     id("maven-publish")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 group = "ru.lavafrai.mai.api"
